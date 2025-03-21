ac6b26892e22b1cee54c93ef85f2ba4a03a0711c
Valeriy Komlev <komlev88@gmail.com>
2025-03-21 20:42:43 +0100

    fix(ui): various UI/UX fixes, landing

--- src/components/02-Organisms/Footer/Footer.tsx
+++ src/components/02-Organisms/Footer/Footer.tsx
@@ -2,35 +2,33 @@ import { GithubIcon } from "../../00-Atoms/Icons/GithubIcon";
 import { ThemeSwitcher } from "../../00-Atoms/ThemeSwitcher/ThemeSwitcher";
 
 export const Footer = () => (
-  <div className="relative">
-    <footer
-      className="relative footer footer-center py-4 bg-neutral z-10"
-      role="contentinfo"
-    >
-      <div className="flex font-medium text-neutral-content">
-        CheckQuest by{" "}
-        <a
-          className="link focusable focusable-neutral"
-          target="_blank"
-          href="https://komlev.me"
-          rel="noopener noreferrer"
-          aria-label="Visit komlev's website"
-        >
-          komlev
-        </a>
-        <a
-          target="_blank"
-          className="link focusable focusable-neutral"
-          href="https://github.com/komlev/checkquest"
-          title="Github link"
-          aria-label="Github link"
-        >
-          <GithubIcon />
-        </a>
-      </div>
-      <div className="absolute right-4">
-        <ThemeSwitcher />
-      </div>
-    </footer>
-  </div>
+  <footer
+    className="footer footer-center py-4 bg-neutral z-10"
+    role="contentinfo"
+  >
+    <div className="flex font-medium text-neutral-content">
+      CheckQuest by{" "}
+      <a
+        className="link focusable focusable-neutral"
+        target="_blank"
+        href="https://komlev.me"
+        rel="noopener noreferrer"
+        aria-label="Visit komlev's website"
+      >
+        komlev
+      </a>
+      <a
+        target="_blank"
+        className="link focusable focusable-neutral"
+        href="https://github.com/komlev/checkquest"
+        title="Github link"
+        aria-label="Github link"
+      >
+        <GithubIcon />
+      </a>
+    </div>
+    <div className="absolute right-4">
+      <ThemeSwitcher />
+    </div>
+  </footer>
 );
