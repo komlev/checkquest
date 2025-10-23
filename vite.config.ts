d7b33327708ab19bc0fe1733978e595c389cfaf1
Valeriy Komlev <komlev88@gmail.com>
2025-10-24 00:41:30 +0200

    feat: add app version into html, add meta info to html

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "vitest/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import pack from "./package.json";
+
+const htmlPlugin = () => {
+  return {
+    name: "html-transform",
+    transformIndexHtml(html: string) {
+      return html.replace(/APP_VERSION/, pack.version);
+    },
+  };
+};
 
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [tailwindcss(), react(), htmlPlugin()],
   test: {
     globals: true,
     environment: "jsdom",
